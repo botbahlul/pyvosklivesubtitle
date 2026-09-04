3ca40adc29a2e11ebb20db4354859a9000af8561
Bot Bahlul <88623122+botbahlul@users.noreply.github.com>
2026-09-04 20:02:22 +0800

    Add files via upload

--- win/mypyinstaller.bat
+++ win/mypyinstaller.bat
@@ -1,3 +1,31 @@
+@echo off
+setlocal
+
+set "folderToDelete1=.\build"
+set "folderToDelete2=.\dist"
+set "fileToDelete1=.\*.spec"
+
+if exist "%folderToDelete1%" (
+    rmdir /s /q "%folderToDelete1%"
+    if errorlevel 1 (
+        echo Error occurred while deleting the folder.
+    )
+)
+
+if exist "%folderToDelete2%" (
+    rmdir /s /q "%folderToDelete2%"
+    if errorlevel 1 (
+        echo Error occurred while deleting the folder.
+    )
+)
+
+if exist "%fileToDelete1%" (
+    del /s /q "%fileToDelete1%"
+    if errorlevel 1 (
+        echo Error occurred while deleting the file.
+    )
+)
+
 pyinstaller ^
 --add-data ".\libgcc_s_seh-1.dll;." ^
 --add-data ".\libstdc++-6.dll;." ^
