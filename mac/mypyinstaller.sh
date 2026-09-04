3e014437802688d0df0a200c1819d32cef80c9c2
Bot Bahlul <88623122+botbahlul@users.noreply.github.com>
2026-09-04 19:54:44 +0800

    Add files via upload

--- mac/mypyinstaller.sh
+++ mac/mypyinstaller.sh
@@ -1,5 +1,21 @@
-pyinstaller --python=/usr/local/bin/python3.8 \
---add-data "./libvosk.so:." \
+folder1="./build"
+folder2="./dist"
+file1="./*.spec"
+
+if [ -d "$folder1" ]; then
+	rm -rf "$folder1"
+fi
+
+if [ -d "$folder2" ]; then
+	rm -rf "$folder2"
+fi
+
+if [ -f "$file1" ]; then
+	rm -f "$file1"
+fi
+
+/usr/local/bin/python3.10 -m PyInstaller \
+--add-data "./libvosk.dyld:." \
 --add-data "./streamlink/plugins/*:streamlink/plugins" \
 --add-data "./streamlink/*:streamlink" \
 --hidden-import argparse \
